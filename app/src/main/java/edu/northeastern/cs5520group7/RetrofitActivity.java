99c48d3621fa27cf84b026ab3d9f14b2389255d7
Emily Li <emily1998ljy@gmail.com>
2022-10-23 15:55:26 -0700

    get comments worked

--- app/src/main/java/edu/northeastern/cs5520group7/RetrofitActivity.java
+++ app/src/main/java/edu/northeastern/cs5520group7/RetrofitActivity.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 import edu.northeastern.cs5520group7.model.Comment;
 import edu.northeastern.cs5520group7.model.IPlaceholder;
-import edu.northeastern.cs5520group7.model.PostModel;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -34,8 +33,8 @@ public class RetrofitActivity extends AppCompatActivity {
         retrofitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getPosts();
-                //getComments();
+                //getPosts();
+                getComments();
                 //postWithQ();
                 //createANewPost();
                 //createANewPostWithHeaders();
@@ -52,55 +51,7 @@ public class RetrofitActivity extends AppCompatActivity {
 
 
     }
-    private void getPosts(){
-        // to execute the call
-        Call<List<PostModel>> call = api.getPostModels();
-        //call.execute() runs on the current thread, which is main at the momement. This will crash
-        // use Retrofit's method enque. This will automaically push the network call to background thread
-        call.enqueue(new Callback<List<PostModel>>() {
-            @Override
-            public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
-                //This gets called when atleast the call reaches a server and there was a response BUT 404 or any legitimate error code from the server, also calls this
-                // check response code is between 200-300 and API was found
 
-                if(!response.isSuccessful()){
-                    Log.d(TAG, "Call failed!" + response.code());
-                    return;
-                }
-
-                Log.d(TAG, "Call Successed!");
-                List<PostModel> postModels = response.body();
-                for(PostModel post : postModels){
-                    StringBuffer  str = new StringBuffer();
-                    str.append("Code:: ")
-                            .append(response.code())
-                            .append("ID : ")
-                            .append(post.getId())
-                            .append("\n")
-                            .append("UserID :")
-                            .append(post.getUserId())
-                            .append("\n")
-                            .append("Title: ")
-                            .append(post.getTitle())
-                            .append("\n")
-                            .append("Description: ")
-                            .append(post.getText())
-                            .append("\n");
-
-
-                    Log.d(TAG, str.toString());
-
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<PostModel>> call, Throwable t) {
-                // this gets called when url is wrong and therefore calls can't be made OR processing the request goes wrong.
-                Log.d(TAG, "Call failed!" + t.getMessage());
-            }
-        });
-    }
     private void getComments() {
         // to execute the call
         //api : https://jsonplaceholder.typicode.com/posts/5/comments
