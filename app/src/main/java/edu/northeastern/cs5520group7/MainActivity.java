5c5d41946dfe4d4d1799cc676953ceb124cd5aa3
Emily Li <emily1998ljy@gmail.com>
2022-10-23 14:45:59 -0700

    add retrofit activity class

--- app/src/main/java/edu/northeastern/cs5520group7/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520group7/MainActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.cs5520group7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void startRetrofitActivity(View view){
+        startActivity(new Intent(MainActivity.this, RetrofitActivity.class));
     }
 }
