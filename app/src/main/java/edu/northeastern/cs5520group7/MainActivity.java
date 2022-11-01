7cd9addde0cbbd46ea016c6c25c83c5ad06194ab
wantingxu <xu.wan@northeastern.edu>
2022-11-01 12:58:39 -0700

    created Stick activity and button

--- app/src/main/java/edu/northeastern/cs5520group7/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520group7/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button stickButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        stickButton = (Button) findViewById(R.id.stickButton);
+        stickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Stick.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+
     }
     public void startRetrofitActivity(View view){
         startActivity(new Intent(MainActivity.this, RetrofitActivity.class));
---
+++ app/src/main/java/edu/northeastern/cs5520group7/Stick.java
@@ -0,0 +1,14 @@
+package edu.northeastern.cs5520group7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stick);
+    }
+}
