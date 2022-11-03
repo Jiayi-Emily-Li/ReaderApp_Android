579bb01a90fa0e9005a3a4d3fbf4a558d4267a3e
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2022-11-03 08:16:16 -1000

    added ChildEventListener

--- app/src/main/java/edu/northeastern/cs5520group7/UserList.java
+++ app/src/main/java/edu/northeastern/cs5520group7/UserList.java
@@ -1,18 +1,32 @@
 package edu.northeastern.cs5520group7;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class UserList extends AppCompatActivity {
-    TextView textview;
+import java.util.ArrayList;
 
-    FirebaseDatabase database;
-    DatabaseReference mRef;
+public class UserList extends AppCompatActivity {
+
+    private static final String TAG = UserList.class.getSimpleName();
+
+    private ListView listView;
+    //set up arrayList to store the data
+    private ArrayList<String> users_Name = new ArrayList<>();
+    private FirebaseDatabase database;
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +35,41 @@ public class UserList extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         mRef=database.getReference("Users");
+
+        //connected element of arrayList to the listView
+        listView = (ListView) findViewById(R.id.UserListView);
+        ArrayAdapter<String> arrAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, users_Name);
+        listView.setAdapter(arrAdapter);
+
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String name = snapshot.getValue(String.class);
+                users_Name.add(name);
+                arrAdapter.notifyDataSetChanged();
+                Log.e(TAG, "onChildAdded: dataSnapshot = " + snapshot.getValue().toString());
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.v(TAG, "onChildChanged: " + snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
