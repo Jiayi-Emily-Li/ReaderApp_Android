6550b117e97cbae10a059beeb45c1db6904a4f87
Emily Li <emily1998ljy@gmail.com>
2022-11-04 10:48:03 -0700

    add User and History classes

---
+++ app/src/main/java/edu/northeastern/cs5520group7/model/History.java
@@ -0,0 +1,51 @@
+package edu.northeastern.cs5520group7.model;
+
+import java.io.Serializable;
+
+public class History implements Serializable {
+    private String from;
+    private String to;
+    private String time;
+    private String image;
+
+    public History(String from, String to, String time, String image) {
+        this.from = from;
+        this.to = to;
+        this.time = time;
+        this.image = image;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
+
---
+++ app/src/main/java/edu/northeastern/cs5520group7/model/User.java
@@ -0,0 +1,61 @@
+package edu.northeastern.cs5520group7.model;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class User {
+    private String userId;
+    private String name;
+    private String token;
+    private ArrayList<History> histories;
+
+    public User() {
+    }
+
+    public User(String userId, String name, String token) {
+        this.userId = userId;
+        this.name = name;
+        this.token = token;
+        this.histories = new ArrayList<>();
+    }
+
+    public User(String userId, String name, String token, ArrayList<History> histories) {
+        this.userId = userId;
+        this.name = name;
+        this.token = token;
+        this.histories = histories;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public ArrayList<History> getHistories() {
+        return histories;
+    }
+
+    public void addHistory(String from, String to, String time, String image) {
+        this.histories.add(new History(from, to, time, image));
+    }
+}
