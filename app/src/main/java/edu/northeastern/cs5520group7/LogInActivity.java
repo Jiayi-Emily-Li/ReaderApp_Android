973408e90c2615eb3a82f2340126276a4505b62a
Emily Li <emily1998ljy@gmail.com>
2022-11-04 11:37:16 -0700

    add database to login activity

--- app/src/main/java/edu/northeastern/cs5520group7/LogInActivity.java
+++ app/src/main/java/edu/northeastern/cs5520group7/LogInActivity.java
@@ -1,18 +1,22 @@
 package edu.northeastern.cs5520group7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LogInActivity extends AppCompatActivity {
 
     String name;
     EditText userName;
     Button submitButton;
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,14 @@ public class LogInActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.userName);
 
         submitButton = (Button) findViewById(R.id.submitButton);
+        userRef = FirebaseDatabase.getInstance().getReference().child("users");
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                  name = userName.getText().toString();
 
                 Intent intent = new Intent(LogInActivity.this, UserHomeActivity.class);
+                intent.putExtra("name", name);
                 LogInActivity.this.startActivity(intent);
 
             }
