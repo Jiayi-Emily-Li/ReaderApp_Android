57379bef7390b01f957e94a686219173560816a3
cytxiwai <98063373+cytxiwai@users.noreply.github.com>
2022-11-05 01:34:27 -1000

    fixed userList - dataSnapshot

--- app/src/main/java/edu/northeastern/cs5520group7/UserList.java
+++ app/src/main/java/edu/northeastern/cs5520group7/UserList.java
@@ -1,6 +1,7 @@
 package edu.northeastern.cs5520group7;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,11 +17,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import edu.northeastern.cs5520group7.Adapter.SelectListener;
 import edu.northeastern.cs5520group7.Adapter.UserAdapter;
+import edu.northeastern.cs5520group7.model.History;
 import edu.northeastern.cs5520group7.model.User;
 
 public class UserList extends AppCompatActivity implements SelectListener {
@@ -37,7 +44,8 @@ public class UserList extends AppCompatActivity implements SelectListener {
         setContentView(R.layout.activity_user_list);
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users");
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference()/*.child("Users")*/;
+
 
 
         //connected element of arrayList to the RecycleView
@@ -46,20 +54,27 @@ public class UserList extends AppCompatActivity implements SelectListener {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         List<User> userList = new ArrayList<>();
+        ArrayList<History> histories = new ArrayList<>();
+
+
 
         dbRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("message for users", snapshot.getValue().toString());
                 userList.clear();
                 for(DataSnapshot Snapshot : snapshot.getChildren()){
-                    User user = snapshot.getValue(User.class);
+                    for(DataSnapshot usersSnapshot: Snapshot.getChildren()) {
+                        String name = usersSnapshot.child("name").getValue().toString();
+                        String userId = usersSnapshot.child("userId").getValue().toString();
+                        String token = usersSnapshot.child("token").getValue().toString();
 
-                    assert user != null && firebaseUser != null;
-                    if(!user.getUserId().equals(firebaseUser.getUid())){
+                        User user = new User(userId, name,token);
                         userList.add(user);
-                    }
-                }
+
+                }}
                 userAdapter = new UserAdapter(UserList.this, userList, UserList.this);
+                recyclerView.setAdapter(userAdapter);
 
             }
 
@@ -78,6 +93,19 @@ public class UserList extends AppCompatActivity implements SelectListener {
     @Override
     public void itemClicked(User user) {
         Toast.makeText(this,"clicked: " + user.getName(),Toast.LENGTH_SHORT).show();
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                sendMessageToDevice(user.getToken());
+            }
+        }).start();
+    }
+
+    private void sendMessageToDevice(String targetToken){
+        JSONObject jPayload = new JSONObject();
+        JSONObject jNotification = new JSONObject();
+        JSONObject jdata = new JSONObject();
 
     }
 }
