ba06126d8204778afb359d04f023aeed2042fd68
wantingxu <xu.wan@northeastern.edu>
2022-11-30 21:54:59 -0800

    add 4 fragments in homepage

--- app/src/main/java/edu/northeastern/cs5520group7/HomePage.java
+++ app/src/main/java/edu/northeastern/cs5520group7/HomePage.java
@@ -1,14 +1,63 @@
 package edu.northeastern.cs5520group7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.northeastern.cs5520group7.databinding.ActivityHomePageBinding;
+
 public class HomePage extends AppCompatActivity {
+
+    private FirebaseUser user;
+    ActivityHomePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.discover:
+                    replaceFragment(new DiscoverFragment());
+                    break;
+                case R.id.myList:
+                    replaceFragment(new ListFragment());
+                    break;
+                case R.id.post:
+                    replaceFragment(new PostFragment());
+                    break;
+
+            }
+
+
+
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+
+
 }
