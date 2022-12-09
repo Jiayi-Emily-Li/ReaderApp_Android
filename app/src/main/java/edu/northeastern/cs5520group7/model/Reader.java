50d1519cf8bf28c4c38d48cedf4f257b3fcebbc8
Emily Li <emily1998ljy@gmail.com>
2022-12-08 20:22:58 -0800

    add functions to reader class

--- app/src/main/java/edu/northeastern/cs5520group7/model/Reader.java
+++ app/src/main/java/edu/northeastern/cs5520group7/model/Reader.java
@@ -1,4 +1,61 @@
 package edu.northeastern.cs5520group7.model;
 
+import java.util.Map;
 public class Reader {
+
+    private String email;
+    private String password;
+    private List<Book> list;
+    private List<Book> discovery;
+    private Map<String, Post> posts;
+
+    public Reader(){
+
+    }
+
+    //initiate a new reader that doesn't have list and posts
+    public Reader(String email, String password, List<Book> discovery){
+        this.email = email;
+        this.password = password;
+        this.discovery = discovery;
+        this.list = new List<Book>();
+        this.posts = new Map<String, Post>();
+    }
+
+    //a reader that doesn't have posts
+    public Reader(String email, String password, List<Book>list, List<Book> discovery){
+        this.email = email;
+        this.password = password;
+        this.discovery = discovery;
+        this.list = list;
+        this.posts = new Map<String, Post>();
+    }
+
+    public Reader(String email, String password, List<Book>list, List<Book> discovery, Map<String, Post> posts){
+        this.email = email;
+        this.password = password;
+        this.discovery = discovery;
+        this.list = list;
+        this.posts = posts;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public Map<String, Post> getPosts(){
+        return posts;
+    }
 }
